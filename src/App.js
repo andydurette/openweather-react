870b0bff53db454ece34bc39a00e17ee33f1fde5
Andy Durette <duretteandy@gmail.com>
2019-11-10 19:28:23 -0500

    Updated styles

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default class App extends Component {
     super(props);
 
     this.state = {
+      // Putting the API Key in state itself is likely bad practice but I'm not working with a backend to hide it so it's just a convient place to store it.
       apiKey: "428bbab3989b31eb5f6dd40e0559cbeb",
       cityName:'Toronto',
       cityId: '',
@@ -21,39 +22,41 @@ export default class App extends Component {
     }
   }
 
+
+
   // Initial Data setup, 
   async componentDidMount() {
     // Call data with the string for the city
+    const locationresponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.cityName}&appid=${this.state.apiKey}`);
+    const locationJson = await locationresponse.json();
+    this.setState({ cityId: locationJson.id });
+    // Call with the cityId data to get it's weather
+    const locationWeatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${this.state.cityId}&APPID=${this.state.apiKey}&units=metric`);
+    const locationWeatherResponseJson = await locationWeatherResponse.json();
+    this.setState({ weather: locationWeatherResponseJson, isFetching: false });
+  }
+
+  // Update weather with API DATA
+  weatherUpdate =  async () => {
+    // Call data with the string for the city
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.cityName}&appid=${this.state.apiKey}`);
     const json = await response.json();
-    this.setState({ cityId: json.id });
-    // Call with the cityId data to get it's weather
-    const response2 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${this.state.cityId}&APPID=${this.state.apiKey}&units=metric`);
-    const json2 = await response2.json();
-    this.setState({ weather: json2, isFetching: false });
+    // If statement to make sure promise resolved with what was asked if not let user know it didn't
+    if(json.name && json.name.toLowerCase() === this.state.cityName.toLowerCase()){
+      this.setState({ cityId: json.id });
+      // Call with the cityId data to get it's weather
+      const response2 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${this.state.cityId}&APPID=${this.state.apiKey}&units=metric`);
+      const json2 = await response2.json();
+      this.setState({ weather: json2, searchErrorValue: "" });
+    }else{
+      this.setState({searchErrorValue: `*City ${this.state.cityName} not found in database.`})
+    }
   }
 
 
   // This is bound to the SearchFilter form input field to update the state
   updateSearch = (event) => {
     this.setState({search: event.target.value.substring(0,30)});
-  }
-
-  // Update weather with API DAT 
-  weatherUpdate =  async (n) => {
-    // Call data with the string for the city
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.cityName}&appid=${this.state.apiKey}`);
-    const json = await response.json();
-    // If statement to make sure promise resolved with what was asked if not let user know it didn't
-   if(json.name && json.name.toLowerCase() === this.state.cityName.toLowerCase()){
-      this.setState({ cityId: json.id });
-      // Call with the cityId data to get it's weather
-      const response2 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${this.state.cityId}&APPID=${this.state.apiKey}&units=metric`);
-      const json2 = await response2.json();
-      this.setState({ weather: json2, searchErrorValue: "" });
-   }else{
-      this.setState({searchErrorValue: `*City ${this.state.cityName} not found in database.`})
-    }
   }
 
   // SearchUpdate function
@@ -65,12 +68,6 @@ export default class App extends Component {
       this.setState({cityName: this.state.search, searchErrorValue: ""}, this.weatherUpdate);
     }
   }
-
-  // API Error Handling
-  componentDidCatch(error, info) {
-    console.log('Oh shit');
-  }
-
 
   render() {
     return (
